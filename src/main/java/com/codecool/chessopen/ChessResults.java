1c21d99412c678a15a2d695a1f15079848bd1397
Adamakos <adamhohmann@gmail.com>
2021-03-24 17:59:43 +0100

    Add files

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,19 +1,40 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.sql.PreparedStatement;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+
+        File file = new File(fileName);
+        Scanner input = null;
+        try {
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        if (input != null) {
+            List<String> names = new ArrayList<>();
+            HashMap<String, Integer> nameAndPoints = new HashMap<>();
+            String[] extracts;
+            Integer sum = 0;
+            while (input.hasNextLine()) {
+                extracts = input.nextLine().split(",");
+                names.add(extracts[0]);
+                for (int i = 1; i < extracts.length; i++) { sum += Integer.parseInt(extracts[i]); }
+                nameAndPoints.put(extracts[0], sum);
+            }
+            input.close();
+            names = nameAndPoints.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .map(Map.Entry::getKey).collect(Collectors.toList());
+            names.sort(Collections.reverseOrder());
+            return names;
+        }
         return null;
     }
 
